09ff3b89e5347b1be5fbcd2d6974555bbb7fb883
Andy Chang <andy@dcard.cc>
2025-01-15 14:00:52 +0800

    chore: module setup

--- app/src/main/java/com/weihua/tintinttest/di/TinTintTestModule.kt
+++ app/src/main/java/com/weihua/tintinttest/di/TinTintTestModule.kt
@@ -1,10 +1,34 @@
 package com.weihua.tintinttest.di
 
+import android.app.Application
+import com.weihua.tintinttest.api.base.ApiDomainContract
+import com.weihua.tintinttest.api.base.defaultOkhttpBuilder
+import com.weihua.tintinttest.api.base.defaultRetrofitBuilder
+import com.weihua.tintinttest.api.jsonplaceholder.JsonPlaceholderApi
+import com.weihua.tintinttest.dao.PhotoDatabase
 import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Module
+import org.koin.core.annotation.Single
 
 @Module
 @ComponentScan("com.weihua.tintinttest")
 class TinTintTestModule {
+    @Single
+    internal fun providerJsonPlaceholderApi(): JsonPlaceholderApi {
+        return defaultRetrofitBuilder
+            .baseUrl(ApiDomainContract.JSON_PLACEHOLDER)
+            .client(
+                defaultOkhttpBuilder.build()
+            )
+            .build()
+            .create(JsonPlaceholderApi::class.java)
+    }
 
+    @Single
+    internal fun providerPhotoDatabase(application: Application): PhotoDatabase {
+        return PhotoDatabase.createInstance(application)
+    }
+
+    @Single
+    fun providerPhotoDao(database: PhotoDatabase) = database.photoDao()
 }
